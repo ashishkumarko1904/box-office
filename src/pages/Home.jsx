67d38b8c4fb23e272ebafb6dd79ae626ec7c6576
Ashish Kumar <120355800+ashishkumarko1904@users.noreply.github.com>
2024-03-18 18:58:19 +0530

    added search,and search for data on btn click

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
-
+import { useState } from "react";
 const Home = ()=>{
+    const [searchStr, setsearchStr] = useState('');
+    const onSearchInputChange = (ev)=>{
+        setsearchStr(ev.target.value);
+    }
+    const onSearch = async(ev)=>{
+        ev.preventDefault();
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`);
+        const body = await response.json();
+        console.log(body);
+    }
     return <div>
-        home
+        <form action="" onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchInputChange} />
+        <button type="submit">Search</button>
+        </form>
     </div>
 }
 export default Home;
