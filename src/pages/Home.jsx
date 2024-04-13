8d1905c2de10d4863de0d4071eb0ea10deaa5a1d
Ashish Kumar <120355800+ashishkumarko1904@users.noreply.github.com>
2024-04-13 17:17:30 +0530

    learned about useReducer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,33 @@
-import { useState } from "react";
+import { useState ,useReducer} from "react";
 import { useQuery } from "@tanstack/react-query";
 import { searchforShows, searchforPeople } from "./../api/tvmaze";
 import SearchForm from "../components/SearchForm";
 import ShowsGrid from "../components/shows/ShowsGrid";
 import ActorsGrid from "../components/actors/ActorsGrid";
+const reducerFn = (currentCounter,action)=>{
+    switch(action.type){
+        case 'INCREMENT': return currentCounter+1;
+        case 'DECREMENT': return currentCounter-1;
+        case 'RESET': return 0;
+        case 'SET_TO': return action.setToValue;
+    }
+    return 0;
+};
 const Home = ()=>{
     const [filter, setFilter] = useState(null)
-
+    const [counter,dispatch] = useReducer(reducerFn,0)
+    const onIncrement = ()=>{
+        dispatch({type: 'INCREMENT'})
+    };
+    const onDecrement = ()=>{
+        dispatch({type: 'DECREMENT'})
+    };
+    const onReset = ()=>{
+        dispatch({type: 'RESET'})
+    };
+    const setTo = ()=>{
+        dispatch({type: 'SET_TO',setToValue: 10})
+    };
     const { data:apiData,error:apiDataError } = useQuery({
       queryKey: ['search', filter],
       queryFn: () => filter.searchOption === 'shows'?searchforShows(filter.q):searchforPeople(filter.q),
@@ -51,6 +72,11 @@ const Home = ()=>{
     }
     return <div>
         <SearchForm onSearch={onSearch} />
+        <div>counter: {counter}</div>
+        <button type="button" onClick={onIncrement}>increment</button>
+        <button type="button" onClick={onDecrement}>decrement</button>
+        <button type="button" onClick={onReset}>reset</button>
+        <button type="button" onClick={setTo}>setto10</button>
          <div>{renderApidata()}</div>
 
     </div>
